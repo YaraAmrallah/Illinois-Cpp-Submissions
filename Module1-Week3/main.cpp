1020039a811cb117a9cbce1153b0d00a69f92945
Yara Amr-Allah <yara.amrallah.94@gmail.com>
2020-05-11 07:23:08 +0200

    Module1-Week3

---
+++ Module1-Week3/main.cpp
@@ -0,0 +1,65 @@
+/*
+ * main.cpp
+ *
+ *  Created on: May 11, 2020
+ *      Author: yaraa
+ */
+
+
+/* Class Pair has already been declared
+as shown in the following comments: */
+
+#include <iostream>
+
+class Pair {
+public:
+  int *pa,*pb;
+  Pair(int a, int b);
+  Pair(const Pair& other);
+ ~Pair();
+};
+
+/* Implement its member functions below. */
+/* Pair Constructor */
+
+Pair :: Pair(int a, int b)
+{
+	pa = new int(a);
+	pb = new int(b);
+}
+
+/* Copy Constructor */
+Pair :: Pair(const Pair& other)
+{
+	pa = new int;
+	*pa = *(other.pa);
+	pb = new int;
+	*pb = *(other.pb);
+}
+
+/* Destructor */
+
+Pair :: ~Pair()
+{
+	delete pa;
+	delete pb;
+}
+
+
+ /* Here is a main() function you can use
+  * to check your implementation of the
+  * class Pair member functions.
+  */
+
+int main() {
+  Pair p(15,16);
+  Pair q(p);
+  Pair *hp = new Pair(23,42);
+  delete hp;
+
+ std::cout << "If this message is printed,"
+    << " at least the program hasn't crashed yet!\n"
+    << "But you may want to print other diagnostic messages too." << std::endl;
+  return 0;
+}
+
